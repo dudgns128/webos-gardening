19d4bf29171c8dfb56115e3f76893a33c27f61b2
hw-ani <pcbmlh73@gmail.com>
2024-03-30 17:31:03 +0900

    Init: Spring boot 프로젝트 생성
    - Spring Initializer(start.spring.io)를 이용한 spring 프로젝트 초기 생성

---
+++ Server/src/main/java/knu_24_1_team10/webosgardening/WebosGardeningApplication.java
@@ -0,0 +1,13 @@
+package knu_24_1_team10.webosgardening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebosGardeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebosGardeningApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/knu_24_1_team10/webosgardening/WebosGardeningApplicationTests.java
@@ -0,0 +1,13 @@
+package knu_24_1_team10.webosgardening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebosGardeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
