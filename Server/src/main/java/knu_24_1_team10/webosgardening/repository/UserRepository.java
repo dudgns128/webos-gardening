be3ed8ceb037dedaff559c9cff32b2e06db264bb
Oh Seung Woo <dhtmddn00@gmail.com>
2024-05-28 23:11:24 +0900

    Transactional annotation 추가

--- Server/src/main/java/knu_24_1_team10/webosgardening/repository/UserRepository.java
+++ Server/src/main/java/knu_24_1_team10/webosgardening/repository/UserRepository.java
@@ -1,13 +1,16 @@
 package knu_24_1_team10.webosgardening.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.EntityManager;
 import knu_24_1_team10.webosgardening.domain.User;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public class UserRepository {
   private final EntityManager em;
 
@@ -23,4 +26,14 @@ public class UserRepository {
   public Optional<User> findById(Long id) {
     return Optional.ofNullable(em.find(User.class, id));
   }
+
+  public Optional<User> findByEmail(String email) {
+    List<User> users = em.createQuery("SELECT u FROM User u WHERE u.email = :email", User.class)
+            .setParameter("email", email)
+            .getResultList();
+    return users.stream().findFirst();
+  }
+
+
+
 }
