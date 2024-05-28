17244893086cd0675608256bdf98c2a64d85f5b0
Oh Seung Woo <dhtmddn00@gmail.com>
2024-05-28 23:08:42 +0900

    Websocket은 모바일로 제어할 때만 사용하게 될 것이기 때문에 그 기능 구현될 때까지 주석처리 해둠.

--- Server/src/main/java/knu_24_1_team10/webosgardening/controller/WebSocketConfig.java
+++ Server/src/main/java/knu_24_1_team10/webosgardening/controller/WebSocketConfig.java
@@ -1,16 +1,23 @@
-package knu_24_1_team10.webosgardening.controller;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-@Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new MyWebSocketHandler(), "/ws").setAllowedOrigins("*");
-    }
-}
+//package knu_24_1_team10.webosgardening.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.config.annotation.EnableWebSocket;
+//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+//import org.springframework.web.socket.handler.TextWebSocketHandler;
+//
+//@Configuration
+//@EnableWebSocket
+//public class WebSocketConfig implements WebSocketConfigurer {
+//
+//    @Autowired
+//    MyWebSocketHandler handler;
+//
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        registry.addHandler(handler, "/ws").setAllowedOrigins("*");
+//    }
+//
+//
+//}
