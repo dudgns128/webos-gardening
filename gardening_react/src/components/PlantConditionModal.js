d1ab568ae385deef94e78be517a3ba3c3fe814fd
dudgns128 <xoals204@gmail.com>
2024-06-12 20:31:49 +0900

    Fix: 센싱 테이더값에 따른 센싱 데이터별 만족도 상태 싱크 맞춤

--- gardening_react/src/components/PlantConditionModal.js
+++ gardening_react/src/components/PlantConditionModal.js
@@ -6,6 +6,10 @@ import './PlantCondition.css';
 const bridge1 = new WebOSServiceBridge();
 const bridge2 = new WebOSServiceBridge();
 
+async function delay() {
+  return new Promise(resolve => setTimeout(resolve, 1));
+}
+
 const PlantConditionModal = ({ isOpen, onClose }) => {
   const [water, setWater] = useState(0);
   const [light, setLight] = useState(0);
@@ -15,7 +19,7 @@ const PlantConditionModal = ({ isOpen, onClose }) => {
   const [lightStatus, setLightStatus] = useState();
   const [temperatureStatus, setTemperatureStatus] = useState();
   const [humidityStatus, setHumidityStatus] = useState();
-  
+  let waterMin, lightMin, temperatureMin, humidityMin, waterMax, lightMax, temperatureMax, humidityMax;
 
   /*이 식에서 min max 값 조절로 부족 과다 적절 표시가능 */
   const getStatus = (value, min, max) => {
@@ -41,7 +45,6 @@ const PlantConditionModal = ({ isOpen, onClose }) => {
     }
   };
 
-
   useEffect(() => {
     if (!isOpen) {
       return;
@@ -50,43 +53,43 @@ const PlantConditionModal = ({ isOpen, onClose }) => {
     const serviceURL2 = "luna://com.team17.homegardening.service/envData";
     bridge2.onservicecallback = function (msg) {
       const response = JSON.parse(msg);
-      setWaterStatus(getStatus(water, response.waterValue-response.waterRange, response.waterValue+response.waterRange));
-      setLightStatus(getStatus(light, response.lightValue-response.lightRange, response.lightValue+response.lightRange));
-      setTemperatureStatus(getStatus(temperature, response.temperatureValue-response.temperatureRange, response.temperatureValue+response.temperatureRange));
-      setHumidityStatus(getStatus(humidity, response.humidityValue-response.humidityRange, response.humidityValue+response.humidityRange));
+      waterMin = response.waterValue-response.waterRange;
+      waterMax = response.waterValue+response.waterRange;
+      lightMin = response.lightValue-response.lightRange;
+      lightMax = response.lightValue+response.lightRange;
+      temperatureMin = response.temperatureValue-response.temperatureRange;
+      temperatureMax = response.temperatureValue+response.temperatureRange;
+      humidityMin = response.humidityValue-response.humidityRange;
+      humidityMax = response.humidityValue+response.humidityRange;
+
+      bridge1.onservicecallback = function (msg) {
+        const response = JSON.parse(msg);
+        if (response.success) {
+          setWater(response.water);
+          setLight(response.light);
+          setTemperature(response.temperature);
+          setHumidity(response.humidity);
+          setWaterStatus(getStatus(response.water, waterMin, waterMax));
+          setLightStatus(getStatus(response.light, lightMin, lightMax));
+          setTemperatureStatus(getStatus(response.temperature, temperatureMin, temperatureMax));
+          setHumidityStatus(getStatus(response.humidity, humidityMin, humidityMax));
+        }
+      };
     };
-    
     bridge2.call(serviceURL2, '{}');
     
     const serviceURL1 = "luna://com.team17.homegardening.service/getSensingData";
-    bridge1.onservicecallback = function (msg) {
-      const response = JSON.parse(msg);
-      if (response.success) {
-        setWater(response.water);
-        setLight(response.light);
-        setTemperature(response.temperature);
-        setHumidity(response.humidity);
-      }
-    };
 
-    bridge1.call(serviceURL1, '{}');
+    bridge1.call(serviceURL1, '{}');  // 처음 센싱 데이터값 바로 노출되도록 추후 개발
     const intervalId1 = setInterval(() => bridge1.call(serviceURL1, '{}'), 3000);
 
-
     return () => {
-      clearInterval(intervalId1);
-      clearInterval(intervalId2);};
+      clearInterval(intervalId1);};
   }, [isOpen]);
 
   if (!isOpen) {
     return null;
   }
-
-  // 예시 적정값 임의로 설정 (value,min,max)
-  // const waterStatus = getStatus(water, 20, 80);
-  // const lightStatus = getStatus(light, 30, 70);
-  // const temperatureStatus = getStatus(temperature, 18, 25);
-  // const humidityStatus = getStatus(humidity, 40, 60);
 
   return (
     <div class="PlantModal">
@@ -103,12 +106,12 @@ const PlantConditionModal = ({ isOpen, onClose }) => {
               <span className={getStatusClass(lightStatus)}>{light} ({lightStatus})</span>
             </div>
             <div className="status-item">
-              <span>습도:</span>
-              <span className={getStatusClass(humidityStatus)}>{humidity} ({humidityStatus})</span>
-            </div>
-            <div className="status-item">
               <span>온도:</span>
               <span className={getStatusClass(temperatureStatus)}>{temperature} ({temperatureStatus})</span>
+            </div>
+            <div className="status-item">
+              <span>습도:</span>
+              <span className={getStatusClass(humidityStatus)}>{humidity} ({humidityStatus})</span>
             </div>
           </div>
         </div>
