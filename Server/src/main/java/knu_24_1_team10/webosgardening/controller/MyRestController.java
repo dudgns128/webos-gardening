a56b2c61ca542e7ad90e4efea3240909f8fd0423
Oh Seung Woo <dhtmddn00@gmail.com>
2024-06-14 20:27:48 +0900

    REST controller /api/userplant 부분 response entity에 id값 추가

--- Server/src/main/java/knu_24_1_team10/webosgardening/controller/MyRestController.java
+++ Server/src/main/java/knu_24_1_team10/webosgardening/controller/MyRestController.java
@@ -1,11 +1,8 @@
 package knu_24_1_team10.webosgardening.controller;
 
-import knu_24_1_team10.webosgardening.domain.PlantInfo;
-import knu_24_1_team10.webosgardening.domain.User;
-import knu_24_1_team10.webosgardening.domain.UserPlant;
-import knu_24_1_team10.webosgardening.repository.PlantInfoRepository;
-import knu_24_1_team10.webosgardening.repository.UserPlantRepository;
-import knu_24_1_team10.webosgardening.repository.UserRepository;
+import knu_24_1_team10.webosgardening.DTO.UserplantResponse;
+import knu_24_1_team10.webosgardening.domain.*;
+import knu_24_1_team10.webosgardening.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,13 +27,11 @@ public class MyRestController {
     // 클라이언트로부터 회원가입 정보를 받아 디비에 등록하는 기능
     @PostMapping("/user")
     public ResponseEntity<String> registerUser(@RequestBody User user) {
-        System.out.println(user.getName());
-
         userRepository.save(user);
         return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
     }
     @PostMapping("/userplant")
-    public ResponseEntity<String> registerUserPlant(
+    public ResponseEntity<UserplantResponse> registerUserPlant(
             @RequestParam("email") String email,
             @RequestParam("password") String password,
             @RequestParam("plantInfoId") Long plantInfoId,
@@ -47,11 +42,9 @@ public class MyRestController {
     ) {
         User user = userRepository.findByEmail(email).orElse(null);
 
-        System.out.println(user.getPassword());
-        System.out.println(password);
         if (password.equals(user.getPassword())==false)
         {
-            return new ResponseEntity<>("email and password not match",HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new UserplantResponse("email and password not match", 0L),HttpStatus.BAD_REQUEST);
         }
 
 
@@ -61,25 +54,28 @@ public class MyRestController {
         if (user == null || plantInfo == null) {
             if (user==null && plantInfo ==null)
             {
-                return new ResponseEntity<>("User and Plantinfo not found", HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>(new UserplantResponse("User and plantinfo not found", 0L), HttpStatus.NOT_FOUND);
 
             }
             else if (user == null)
             {
-                return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>(new UserplantResponse("User not found", 0L), HttpStatus.NOT_FOUND);
             }
             else
             {
-                return new ResponseEntity<>("Plantinfo not found", HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>(new UserplantResponse("plantinfo not found", 0L), HttpStatus.NOT_FOUND);
             }
         }
 
-
         UserPlant userPlant = new UserPlant(user, plantInfo, name, birthDate, isAutoControl, level);
 
-        userPlantRepository.save(userPlant);
 
-        return new ResponseEntity<>("UserPlant registered successfully", HttpStatus.CREATED);
+
+        userPlant=userPlantRepository.save(userPlant);
+
+        UserplantResponse response= new UserplantResponse("UserPlant registered successfully", userPlant.getId());
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     // 클라이언트에게 모든 식물 정보를 조회하여 보내는 기능
@@ -88,5 +84,6 @@ public class MyRestController {
         List<PlantInfo> plantInfoList = plantInfoRepository.findAll();
         return new ResponseEntity<>(plantInfoList, HttpStatus.OK);
     }
+
 }
 
