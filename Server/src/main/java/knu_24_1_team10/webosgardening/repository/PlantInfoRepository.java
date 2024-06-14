e7ecd20518b9c8fd8f7ded839f7484710fca7991
Oh Seung Woo <dhtmddn00@gmail.com>
2024-06-14 20:28:21 +0900

    plantinfo repository 를 jpa repostiory로 변경

--- Server/src/main/java/knu_24_1_team10/webosgardening/repository/PlantInfoRepository.java
+++ Server/src/main/java/knu_24_1_team10/webosgardening/repository/PlantInfoRepository.java
@@ -3,26 +3,14 @@ package knu_24_1_team10.webosgardening.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.EntityManager;
 import knu_24_1_team10.webosgardening.domain.PlantInfo;
 
 @Repository
-public class PlantInfoRepository {
-  private final EntityManager em;
+public interface PlantInfoRepository extends JpaRepository<PlantInfo,Long> {
 
-  public PlantInfoRepository(EntityManager em) {
-    this.em = em;
-  }
-
-  public Optional<PlantInfo> findById(Long id) {
-    return Optional.ofNullable(em.find(PlantInfo.class, id));
-  }
-
-  public List<PlantInfo> findAll() {
-
-      return null;
-  }
 
 }
